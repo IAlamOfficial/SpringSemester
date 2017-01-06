458e3f10ebeb5a790720860a90387f21b8383f77
Student 6 <Student 6@10.8.33.149>
2017-01-06 13:15:45 -0500

    edited part 3

--- SpringSemester/src/simon/SimonScreenAlam.java
+++ SpringSemester/src/simon/SimonScreenAlam.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class SimonScreenAlam implements Runnable {
 
 	private static String Textlabel;
-	private static String ButtonInterfaceAlam;
-	private static String ProgressinterfaceAlam;
+	private static ButtonInterfaceAlam buttonInterface;
+	private static ProgressInterfaceAlam progressInterface;
 	private ArrayList<MoveInterfaceAlam> arraylist;
 	public SimonScreenAlam() {
 		// TODO Auto-generated constructor stub
