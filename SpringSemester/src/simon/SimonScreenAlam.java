7a77cad1b881e89adbe7e904c5e7d376cb905234
Student 6 <Student 6@10.8.33.149>
2017-01-06 13:07:46 -0500

    Started step 3

---
+++ SpringSemester/src/simon/SimonScreenAlam.java
@@ -0,0 +1,21 @@
+package simon;
+
+import java.util.ArrayList;
+
+public class SimonScreenAlam implements Runnable {
+
+	private static String Textlabel;
+	private static String ButtonInterfaceAlam;
+	private static String ProgressinterfaceAlam;
+	private ArrayList<MoveInterfaceAlam> arraylist;
+	public SimonScreenAlam() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
